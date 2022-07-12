752bf95a4ae9b8dbcd7054f0927a1d8dfae255f1
Hubert Lipiński <hubertlipinskipl@gmail.com>
2022-07-12 18:12:57 +0200

    landing

---
+++ components/LaunchTimer.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="w-full flex flex-row justify-between">
+    <div class="flex flex-col text-center">
+      <p class="timer-value">01</p>
+      <p class="timer-caption">days</p>
+    </div>
+    <div class="flex flex-col text-center">
+      <p class="timer-value">12</p>
+      <p class="timer-caption">hours</p>
+    </div>
+    <div class="flex flex-col text-center">
+      <p class="timer-value">07</p>
+      <p class="timer-caption">minutes</p>
+    </div>
+    <div class="flex flex-col text-center">
+      <p class="timer-value">59</p>
+      <p class="timer-caption">seconds</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LaunchTimer"
+}
+</script>
+
+<style scoped>
+  .timer-value {
+    @apply text-[110px] leading-[90px] text-black;
+  }
+  .timer-caption{
+    @apply text-[30px] leading-[85px] text-gray-500 uppercase;
+  }
+</style>
---
+++ components/TheLanding.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="hero-image -mt-32 flex">
+    <div class="container mx-auto flex items-center">
+      <div class="h-2/3 w-7/12 flex flex-col justify-around gap-5">
+        <header>
+          <p class="subtext text-gray">Next mission launch</p>
+          <h1 class="jumbotron">Starlink 4-21 (v1.5)</h1>
+        </header>
+        <LaunchTimer/>
+        <div class="btn w-[225px] h-[70px] bg-primary text-white rounded-[25px] text-center text-[22px] leading-[70px]">Learn more</div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.hero-image {
+  @apply bg-gray-50 w-full h-screen;
+  background: url('./static/landing.png') center;
+  background-size: cover;
+}
+</style>
+
